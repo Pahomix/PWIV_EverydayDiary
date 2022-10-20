fa43c674d291c32b39d599cf0cf119d7f12dc2fd
Pahomix <pahomix@inbox.ru>
2022-10-20 22:05:24 +0300

    Fatatl error if you open

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork_IV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DiaryMenu.Page(0);
+            Arrows();
+        }
+
+        public static void Arrows()
+        {
+            ConsoleKeyInfo arrow = Console.ReadKey();
+            while (true)
+            {
+
+                switch (arrow.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        DiaryMenu.Menu(arrow);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        DiaryMenu.Menu(arrow);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        DiaryMenu.ChangePosition(arrow);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        DiaryMenu.ChangePosition(arrow);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Ошибка");
+                        break;
+                }
+                if (arrow.Key == ConsoleKey.Escape) Environment.Exit(0);
+            }
+        }
+    }
+}
